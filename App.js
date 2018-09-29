dbdc7b440cf14432f72790480f8c7e1606a68c08
Ricardo Baquero <wola_ricky@hotmail.com>
2018-09-29 14:05:26 -0500

    - Navigation module finished.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import {Navigation} from 'react-native-navigation';
 import {Provider} from 'react-redux';
 
+import configureStore from './src/store/configureStore';
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
 import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
-
-import configureStore from './src/store/configureStore';
+import PlaceDetailScreen from "./src/screens/PlaceDetail/PlaceDetail";
+import SideDrawer from "./src/screens/SideDrawer/SideDrawer";
 
 const store = configureStore();
 
@@ -25,6 +26,18 @@ Navigation.registerComponent(
 Navigation.registerComponent(
     "awesome-places.FindPlaceScreen",
     () => FindPlaceScreen,
+    store,
+    Provider
+);
+Navigation.registerComponent(
+    "awesome-places.PlaceDetailScreen",
+    () => PlaceDetailScreen,
+    store,
+    Provider
+);
+Navigation.registerComponent(
+    "awesome-places.SideDrawer",
+    () => SideDrawer,
     store,
     Provider
 );
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,16 +1,41 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
 import {Text, View} from "react-native";
 
+import PlaceInput from '../../components/PlaceInput/PlaceInput';
+import * as actions from '../../store/actions';
+
 class SharePlaceScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+    }
+
+    onNavigatorEvent = event => {
+        if (event.type === "NavBarButtonPress") {
+            if (event.id === "sideDrawerToggle") {
+                this.props.navigator.toggleDrawer({
+                    side: 'left'
+                });
+            }
+        }
+    };
+
+    placeAddedHandler = placeName => {
+        this.props.onAddPlace(placeName)
+    };
 
     render() {
         return (
             <View>
-                <Text>On SharePlaceScreen</Text>
+                <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
             </View>
         );
     }
 
 }
 
-export default SharePlaceScreen;
+export default connect(null, {
+    onAddPlace: actions.addPlace
+})(SharePlaceScreen);
