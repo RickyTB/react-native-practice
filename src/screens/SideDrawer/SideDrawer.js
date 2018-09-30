c24b6cf68f58da47b79e44d0669bfc5f895421ac
Ricardo Baquero <wola_ricky@hotmail.com>
2018-09-30 02:54:15 -0500

    - Responsive layout finished.

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, Dimensions, StyleSheet, TouchableOpacity} from "react-native";
+import {Text, View, Dimensions, StyleSheet, TouchableOpacity, Platform} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
 class SideDrawer extends Component {
@@ -8,7 +8,7 @@ class SideDrawer extends Component {
             <View style={[styles.container, {width: Dimensions.get('window').width * 0.8}]}>
                 <TouchableOpacity>
                     <View style={styles.drawerItem}>
-                        <Icon style={styles.drawerItemIcon} name="ios-log-out" size={30} color="#aaa"/>
+                        <Icon style={styles.drawerItemIcon} name={Platform.OS === "android" ? "md-log-out" : "ios-log-out"} size={30} color="#aaa"/>
                         <Text>Sign Out</Text>
                     </View>
                 </TouchableOpacity>
